3315b3994dc6ca17c34215a0447d873f5bd55b8a
ffarahhabib <farah.habib@student.giu-uni.de>
2025-11-12 15:13:43 +0200

    reptile in header

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,17 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+//
+// Created by Farah Habib on 12/11/2025.
+//
 
-#include "Animal.h"
+#ifndef UNTITLED3_REPTILE_H
+#define UNTITLED3_REPTILE_H
+#include <__fwd/string.h>
 
-//define reptile class here
-
-
-
-
-
-
-
-
-
-#endif
+class Reptile:public Animal {
+private:
+    bool isVenomous;
+    public:
+    Reptile();
+    Reptile(bool isVenomous);
+    ~Reptile();
+};
+#endif //UNTITLED3_REPTILE_H
