adfaa0ac3da486c80410179e7c2e9806f435a089
ffarahhabib <farah.habib@student.giu-uni.de>
2025-11-12 15:14:10 +0200

    Refactor Visitor.h with updated header guard
    Updated header guard and included forward declaration for string.

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,19 @@
-#ifndef VISITOR_H
-#define VISITOR_H
+//
+// Created by Farah Habib on 12/11/2025.
+//
 
-#include <string>
-using namespace std;
+#ifndef UNTITLED3_VISITOR_H
+#define UNTITLED3_VISITOR_H
+#include <__fwd/string.h>
 
-//define visitor class here
-
-
-
-
-
-
-
-#endif
+class Visitor {
+    private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    Visitor(string visitorName, int ticketsBought);
+    virtual ~Visitor();
+    void displayInfo();
+};
+#endif //UNTITLED3_VISITOR_H
