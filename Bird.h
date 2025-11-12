eb9f459d425f75248e997739cb6d8269a99c3817
ffarahhabib <farah.habib@student.giu-uni.de>
2025-11-12 15:11:41 +0200

    Create Bird class in Bird.h
    Define the Bird class with constructors and destructor.

--- Bird.h
+++ Bird.h
@@ -1,14 +1,15 @@
-#ifndef BIRD_H
-#define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
 
 
+#ifndef UNTITLED3_BIRD_H
+#define UNTITLED3_BIRD_H
+#include <string>
 
-
-
-
-
-#endif
+class Bird:public Animal {
+private:
+    float wingSpan;
+    public:
+    Bird();
+    Bird(float wingSpan);
+    ~Bird();
+};
+#endif //UNTITLED3_BIRD_H
